231d2b23a75ed38c39b1c45e9f58bcba8476dca6
melissa aa <melissa_ar_ac@hotmail.com>
2015-06-20 14:56:59 -0600

    Creado y agregado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astrus::Application.config.secret_token = 'aa2eae53f9f007af5a7c66ed87760f234f4ca2460bac007f42456fb291cd67845c8b354375d5cf86383ec990293e02ceeb1176c50342d43ddd1c1615adf70520'
